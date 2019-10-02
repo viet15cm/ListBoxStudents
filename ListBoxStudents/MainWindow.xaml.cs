3a07824b89ddf8934c11a0d103d38eba7506749a
Administrator <Administrator@SHARE10S-PC>
2019-10-02 08:42:48 +0700

    addding someting

---
+++ ListBoxStudents/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ListBoxStudents;
+namespace ListBoxStudents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+      
+        List<Student> students;
+        List<string> nameList;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            string name = "Việt Nam Tùng Chiến Hà Vỹ Lộ Tính Toan Tú Linh Thịnh Nhóc Bảo Tuyền Quân Công ";
+            String[] str = name.Split(' ');
+            students = new List<Student>();
+
+            for(int i=0; i< str.Length; i++)
+            {
+                students.Add(new Student(str[i]));
+            }
+
+            nameList = new List<string>();
+
+            for(int i=0; i<students.Count; i++)
+            {
+                nameList.Add(i + "\t" + students[i].Ten);
+            }
+
+            
+            lsbList.ItemsSource = nameList;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ListBoxStudents/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListBoxStudents
+{
+    class Student
+    {
+        private string ten;
+
+        public Student(string ten)
+        {
+            this.Ten = ten;
+        }
+
+        public string Ten { get => ten; set => ten = value; }
+    }
+}
